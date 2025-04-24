ca8ef1b88ad36e61d257e59cb37315d1bb210d1d
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-04-23 23:27:03 -0700

    adde m

--- middleware.ts
+++ middleware.ts
@@ -6,8 +6,10 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
+    cookieName: process.env.NODE_ENV === 'production'
+      ? '__Secure-next-auth.session-token'
+      : 'next-auth.session-token',
   })
-
   // Check if the user is authenticated
   const isAuthenticated = !!token
 
